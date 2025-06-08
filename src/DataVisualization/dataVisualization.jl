d56ed41edde98511593fa4358167814df590a0e1
SebastianVera <svera@fi.uba.ar>
2025-06-08 17:14:00 -0300

    Se termina la limpieza de datos y se arman los plots

--- src/DataVisualization/dataVisualization.jl
+++ src/DataVisualization/dataVisualization.jl
@@ -1,7 +1,20 @@
 module DataVisualization
     using CSV
     using DataFrames
+    using Statistics
     using Plots
+
+    function completarConPromedio(df:: DataFrame, grupo::Symbol, columna::Symbol)
+        promedios = combine(groupby(df, grupo), columna => mean ∘ skipmissing => :promedio)
+
+        df = leftjoin(df, promedios, on = grupo)
+    
+        df[!, columna] = coalesce.(df[!, columna], df[!, :promedio])
+    
+        select!(df, Not(:promedio))
+    
+        return df
+    end
 
     function clearDataSet()
         df = CSV.read("../../dataSet/properati_argentina_2021.csv", DataFrame)
@@ -11,19 +24,19 @@ module DataVisualization
 
         #= Se cuenta la cantidad de missing data de las columnas =#
         missing_counts = Dict(name => count(ismissing, df[!, name]) for name in names(df))
-        println(missing_counts)
+        #= println(missing_counts) =#
 
         #= Se cuenta la cantidad de variedad de data que tiene opertarion =#
         opreacionCantidad = combine(groupby(df, :operation), nrow => :cantidad)
-        println(opreacionCantidad)
+        #= println(opreacionCantidad) =#
         
         #= Se cuenta la cantidad de variedad de data que tiene place_l2 =#
         placeCantidad = combine(groupby(df, :place_l2), nrow => :cantidad)
-        println(placeCantidad)
+        #= println(placeCantidad) =#
 
         #= Se cuenta la cantidad de variedad de data que tiene property_currency =#
         currencyCant = combine(groupby(df, :property_currency), nrow => :cantidad)
-        println(currencyCant)
+        #= println(currencyCant) =#
 
         #=
             Se elimina place_l4 al ver que tiene un gran porcentaje de columnas con missing data 
@@ -33,12 +46,50 @@ module DataVisualization
             Se elimina property_currency ya que solo posee el valor USD
             Se eliminan start_date, end_date y created_on
         =#
-        select!(df, Not(:place_l4, :Column1, :id, :operation, :place_l2, :property_title, :property_currency, :start_date, :end_date, :created_on))
+        select!(df, Not(:place_l4, :Column1, :id, :operation, :place_l2, :property_title, :property_currency, :start_date, :end_date, :created_on, :property_bedrooms))
+
+        #= Se eliminan los missing de las siguientes columnas =#
+        dropmissing!(df, [:latitud, :longitud, :place_l3])
+
+        #= Se calcula el promedio de property_surface_total para los tipos de propiedades y se completa los missing con este valor =#
+        df = completarConPromedio(df, :property_type, :property_surface_total)
+        
+        #= Se calcula el promedio de property_surface_covered para los tipos de propiedades y se completa los missing con este valor =#
+        df = completarConPromedio(df, :property_type, :property_surface_covered)
+
+        return df
+    end
+
+    function plotPropertyType(df:: DataFrame)
+        countType = combine(groupby(df, [:property_type]), nrow => :cantidad)
+
+        graf1 = bar(countType.property_type, countType.cantidad, legend = false, title ="Tipos de propiedades en venta en Capital Federal en el año 2021")
+
+        display(graf1)
+    end
+
+    function plotMapPropertys(df:: DataFrame)
+        tipos = unique(df.property_type)
+
+        # Asignar un color a cada tipo
+        color_map = Dict(t => i for (i, t) in enumerate(tipos))
+        colors = [color_map[t] for t in df.property_type]
+
+        # Graficar
+        scatter(df.longitud, df.latitud;
+            group = df.property_type,
+            legend = :topleft,
+            title = "Propiedades en Capital Federal",
+            xlabel = "Longitud", ylabel = "Latitud",
+            markersize = 5,
+            palette = :viridis)
     end
 
     export dataVisualization
     function dataVisualization()
-        clearDataSet()
+        df = clearDataSet()
+        plotPropertyType(df)
+        plotMapPropertys(df)
     end
 
 end
