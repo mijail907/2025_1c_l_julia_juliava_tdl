ffa97c65ac4db1752b0a7a22731ad5ae0df84256
Mijail Braian Arispe <marispe@fi.uba.ar>
2025-06-29 23:03:08 -0300

    se eliminaron lineas de codigo comentada en buscarPrimos.jl

--- src/encontrarNumerosPrimos/buscarPrimos.jl
+++ src/encontrarNumerosPrimos/buscarPrimos.jl
@@ -71,60 +71,22 @@ end
 """
 Compara tiempos de ejecución entre versión secuencial y paralela.
 """
-function test_paralelismo()
-    desde = 1
-    hasta = 20_000_000
+    function test_paralelismo()
+        desde = 1
+        hasta = 20_000_000
 
-    println(" Búsqueda secuencial:")
-    t1 = time()
-    primos_seq = encontrar_primos(desde, hasta)
-    tiempo_seq = time() - t1
-    println(" Tiempo secuencial: $(round(tiempo_seq, digits=3)) segundos")
+        println(" Búsqueda secuencial:")
+        t1 = time()
+        primos_seq = encontrar_primos(desde, hasta)
+        tiempo_seq = time() - t1
+        println(" Tiempo secuencial: $(round(tiempo_seq, digits=3)) segundos")
 
-    println("\nBúsqueda paralela:")
-    t2 = time()
-    primos_par = encontrar_primos_parallel(desde, hasta)
-    tiempo_par = time() - t2
-    println("Tiempo paralelo: $(round(tiempo_par, digits=3)) segundos")
+        println("\nBúsqueda paralela:")
+        t2 = time()
+        primos_par = encontrar_primos_parallel(desde, hasta)
+        tiempo_par = time() - t2
+        println("Tiempo paralelo: $(round(tiempo_par, digits=3)) segundos")
 
-    println("\n¿Resultados iguales? ", primos_seq == primos_par)
-end
-
-    # function encontrar_primos_parallel(desde::Int, hasta::Int)
-    #     medio = (desde + hasta) ÷ 2
-
-    #     t1 = Threads.@spawn begin
-    #         println("Hilo 1 buscando de $desde a $medio (Thread $(Threads.threadid()))")
-    #         # sleep(0.5)
-    #         encontrar_primos(desde, medio)
-    #     end
-
-    #     t2 = Threads.@spawn begin
-    #         println("Hilo 2 buscando de $(medio+1) a $hasta (Thread $(Threads.threadid()))")
-    #         # sleep(0.5)
-    #         encontrar_primos(medio+1, hasta)
-    #     end
-
-    #     return sort(vcat(fetch(t1), fetch(t2)))
-    # end
-
-    # function test_paralelismo()
-    #     desde = 1
-    #     hasta = 20_000_000
-
-    #     println("Búsqueda secuencial:")
-    #     t1 = time()
-    #     primos_seq = encontrar_primos(desde, hasta)
-    #     tiempo_seq = time() - t1
-    #     println(" Tiempo secuencial: $(round(tiempo_seq, digits=3)) segundos")
-
-    #     println("\n Búsqueda paralela:")
-    #     t2 = time()
-    #     primos_par = encontrar_primos_parallel(desde, hasta)
-    #     tiempo_par = time() - t2
-    #     println(" Tiempo paralelo: $(round(tiempo_par, digits=3)) segundos")
-
-    #     println("\n ¿Resultados iguales? ", primos_seq == primos_par)
-    # end
-
+        println("\n¿Resultados iguales? ", primos_seq == primos_par)
+    end
 end # module
